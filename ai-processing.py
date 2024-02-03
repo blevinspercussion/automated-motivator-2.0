7bd8e5f124897c66cc7aa53f7ba6549f71cd233b
Adam Blevins <blevinspercussion@gmail.com>
2024-02-03 12:05:55 -0500

    Refactored ai-processing.py into a class

--- ai-processing.py
+++ ai-processing.py
@@ -5,154 +5,165 @@ from base64 import b64decode
 client = OpenAI(api_key="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")
 
 
-def construct_image_prompt():
-    """
-    Constructs a pseudo-random image prompt for an ai model, and returns the prompt as a string.
-    """
+class ai_processing:
+    def __init__(self, client):
+        self.client = client
 
-    PICTURE_TYPES = [
-        "sky",
-        "nature",
-        "woods",
-        "outer space",
-        "ocean",
-        "flowers",
-        "gradient color",
-        "liminal space",
-        "lake",
-        "pond",
-        "forest",
-        "water",
-        "trees",
-        "desert",
-    ]
+    def construct_image_prompt():
+        """
+        Constructs a pseudo-random image prompt for an ai model, and returns the prompt as a string.
+        """
 
-    WEATHER_TYPES = [
-        "clear",
-        "cloudy",
-        "rainy",
-        "overcast",
-        "stormy",
-        "sunny",
-        "bright",
-        "gloomy",
-    ]
+        PICTURE_TYPES = [
+            "sky",
+            "nature",
+            "woods",
+            "outer space",
+            "ocean",
+            "flowers",
+            "gradient color",
+            "liminal space",
+            "lake",
+            "pond",
+            "forest",
+            "water",
+            "trees",
+            "desert",
+        ]
 
-    STYLE_TYPES = [
-        "anime",
-        "photographic",
-        "painted",
-        "hand drawn",
-        "hand painted",
-        "pixel art",
-        "oil painting",
-        "science fiction",
-        "fantasy",
-        "synthwave",
-        "cyberpunk",
-        "digital art",
-        "crayon",
-        "single line drawing",
-        "3d rendered",
-    ]
+        WEATHER_TYPES = [
+            "clear",
+            "cloudy",
+            "rainy",
+            "overcast",
+            "stormy",
+            "sunny",
+            "bright",
+            "gloomy",
+        ]
 
-    TIME_OF_DAY = ["morning", "day", "afternoon", "night", "evening", "dawn", "dusk"]
+        STYLE_TYPES = [
+            "anime",
+            "photographic",
+            "painted",
+            "hand drawn",
+            "hand painted",
+            "pixel art",
+            "oil painting",
+            "science fiction",
+            "fantasy",
+            "synthwave",
+            "cyberpunk",
+            "digital art",
+            "crayon",
+            "single line drawing",
+            "3d rendered",
+        ]
 
-    return f"{random.choice(PICTURE_TYPES)} on a {random.choice(WEATHER_TYPES)} {random.choice(TIME_OF_DAY)} in a {random.choice(STYLE_TYPES)} style"
+        TIME_OF_DAY = [
+            "morning",
+            "day",
+            "afternoon",
+            "night",
+            "evening",
+            "dawn",
+            "dusk",
+        ]
 
+        return f"{random.choice(PICTURE_TYPES)} on a {random.choice(WEATHER_TYPES)} {random.choice(TIME_OF_DAY)} in a {random.choice(STYLE_TYPES)} style"
 
-def construct_text_prompt():
-    """
-    Constructs a pseudo-random prompt for an ai to generate a quote and returns the prompt as a string.
-    """
+    def construct_text_prompt():
+        """
+        Constructs a pseudo-random prompt for an ai to generate a quote and returns the prompt as a string.
+        """
 
-    QUOTE_TYPE = [
-        "inspirational",
-        "motivational",
-        "sympathetic",
-        "heartfelt",
-        "happy",
-        "friendly",
-        "introspective",
-        "reflective",
-        "nostalgic",
-    ]
+        QUOTE_TYPE = [
+            "inspirational",
+            "motivational",
+            "sympathetic",
+            "heartfelt",
+            "happy",
+            "friendly",
+            "introspective",
+            "reflective",
+            "nostalgic",
+        ]
 
-    QUOTE_TOPICS = [
-        "love",
-        "determination",
-        "hard work",
-        "perserverence",
-        "gratitude",
-        "friendship",
-        "happiness",
-        "success",
-        "generosity",
-        "loss",
-        "adversity",
-        "music",
-        "family",
-        "loneliness",
-        "home",
-        "simple living",
-        "time",
-        "life",
-        "mental health",
-        "the future",
-        "lost love",
-        "new beginnings",
-        "humility",
-    ]
+        QUOTE_TOPICS = [
+            "love",
+            "determination",
+            "hard work",
+            "perserverence",
+            "gratitude",
+            "friendship",
+            "happiness",
+            "success",
+            "generosity",
+            "loss",
+            "adversity",
+            "music",
+            "family",
+            "loneliness",
+            "home",
+            "simple living",
+            "time",
+            "life",
+            "mental health",
+            "the future",
+            "lost love",
+            "new beginnings",
+            "humility",
+        ]
 
-    return (
-        f"make a {random.choice(QUOTE_TYPE)} quote about {random.choice(QUOTE_TOPICS)}"
-    )
+        return f"make a {random.choice(QUOTE_TYPE)} quote about {random.choice(QUOTE_TOPICS)}"
 
+    def generate_image(image_prompt, client):
+        """
+        Takes a prompt as an argument and uses it to generate an ai image using OpenAI Dall-E 3.
+        Returns the image data as Base64 JSON.
+        """
 
-def generate_image(image_prompt):
-    """
-    Takes a prompt as an argument and uses it to generate an ai image using OpenAI Dall-E 3.
-    Returns the image data as Base64 JSON.
-    """
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=image_prompt,
+            size="1024x1024",
+            quality="standard",
+            response_format="b64_json",
+            n=1,  # number of images generated (dall-e-3 only supports 1)
+        )
 
-    response = client.images.generate(
-        model="dall-e-3",
-        prompt=image_prompt,
-        size="1024x1024",
-        quality="standard",
-        response_format="b64_json",
-        n=1,  # number of images generated (dall-e-3 only supports 1)
-    )
+        return response
 
-    return response
+    def generate_quote(quote_prompt, client):
+        """
+        Takes a prompt as an argument and uses it to create a quote using OpenAI GPT 3.5 Turbo
+        """
 
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a brilliant poet and orator that loves dispensing wisdom.",
+                },
+                {"role": "user", "content": quote_prompt},
+            ],
+        )
 
-def generate_quote(quote_prompt):
-    """
-    Takes a prompt as an argument and uses it to create a quote using OpenAI GPT 3.5 Turbo
-    """
+        quote = response.choices[0].message.content
 
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a brilliant poet and orator that loves dispensing wisdom.",
-            },
-            {"role": "user", "content": quote_prompt},
-        ],
-    )
-
-    quote = response.choices[0].message.content
-
-    print(quote)
-    return quote
+        print(quote)
+        return quote
 
 
 def main():
-    print(construct_image_prompt(), "\n", construct_text_prompt())
-    generate_image(construct_image_prompt())
-    generate_quote(construct_text_prompt())
+    print(
+        ai_processing.construct_image_prompt(),
+        "\n",
+        ai_processing.construct_text_prompt(),
+    )
+    ai_processing.generate_image(ai_processing.construct_image_prompt(), client)
+    ai_processing.generate_quote(ai_processing.construct_text_prompt(), client)
 
 
 if __name__ == "__main__":
